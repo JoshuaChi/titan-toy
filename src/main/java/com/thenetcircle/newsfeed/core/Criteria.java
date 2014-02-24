423a91baf47efde16947d4b729f96d69b2ade176
Joshua Chi <jchi@thenetcircle.com>
2014-02-24 18:10:20 +0800

    apply out edges to Criteria class

--- src/main/java/com/thenetcircle/newsfeed/core/Criteria.java
+++ src/main/java/com/thenetcircle/newsfeed/core/Criteria.java
@@ -1,13 +1,35 @@
 package com.thenetcircle.newsfeed.core;
 
+import java.util.List;
 import java.util.Map;
+
+import com.tinkerpop.blueprints.Vertex;
 
 public class Criteria {
 
-	private int offset;
-	private int limit;
-	private GeoCriteria center;
+	private int offset = 0;
+	private int limit = 10;
+	private GeoCriteria center = null;
+	private Vertex startVertex = null;
 	
+	private List<String> edges = null;
+
+	public List<String> getEdges() {
+		return edges;
+	}
+
+	public void setEdges(List<String> edges) {
+		this.edges = edges;
+	}
+
+	public Vertex getStartVertex() {
+		return startVertex;
+	}
+
+	public void setStartVertex(Vertex startVertex) {
+		this.startVertex = startVertex;
+	}
+
 	private Map<String, Object> properties;
 
 	public int getOffset() {
@@ -41,6 +63,5 @@ public class Criteria {
 	public void setProperties(Map<String, Object> properties) {
 		this.properties = properties;
 	}
-	
-	
+
 }
--- src/main/java/com/thenetcircle/newsfeed/core/impl/NewsfeedCore.java
+++ src/main/java/com/thenetcircle/newsfeed/core/impl/NewsfeedCore.java
@@ -3,26 +3,72 @@
  */
 package com.thenetcircle.newsfeed.core.impl;
 
+import java.util.Iterator;
 import java.util.List;
 
+import com.thenetcircle.newsfeed.EdgeType;
+import com.thenetcircle.newsfeed.Property;
 import com.thenetcircle.newsfeed.core.Criteria;
 import com.thenetcircle.newsfeed.core.INewsfeedCore;
 import com.thenetcircle.newsfeed.exceptions.NewsfeedException;
+import com.thinkaurelius.titan.core.TitanGraph;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
+import com.tinkerpop.pipes.PipeFunction;
+import com.tinkerpop.pipes.util.structures.Pair;
 
 /**
  * @author jchi
- *
+ * 
  */
 public class NewsfeedCore implements INewsfeedCore {
+	private TitanGraph g = null;
 
-	/* (non-Javadoc)
-	 * @see com.thenetcircle.newsfeed.core.INewsfeedCore#filter(com.thenetcircle.newsfeed.core.Criteria)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.thenetcircle.newsfeed.core.INewsfeedCore#filter(com.thenetcircle.
+	 * newsfeed.core.Criteria)
 	 */
 	@Override
 	public List<Vertex> filter(Criteria c) throws NewsfeedException {
-		// TODO Auto-generated method stub
-		return null;
+		int offset = c.getOffset();
+		int limit = c.getLimit();
+		GremlinPipeline<Object, Vertex> gp = new GremlinPipeline<Object, Vertex>(
+				g);
+
+		if (c.getStartVertex() != null) {
+			gp = gp.start(c.getStartVertex()).cast(Vertex.class);
+		}
+
+		List<String> edges = c.getEdges();
+		if (edges != null) {
+			for (String e : edges) {
+				gp = gp.out(e);
+			}
+		}
+		List<Vertex> result = gp
+				.order(new PipeFunction<Pair<Vertex, Vertex>, Integer>() {
+					@Override
+					public Integer compute(Pair<Vertex, Vertex> argument) {
+						Integer iB = new Integer(argument.getB()
+								.getProperty(Property.Time.TIMESTAMP)
+								.toString());
+						Integer iA = new Integer(argument.getA()
+								.getProperty(Property.Time.TIMESTAMP)
+								.toString());
+						return iB.compareTo(iA);
+					}
+
+				}).range(offset, limit).toList();
+
+		return result;
+	}
+
+	public NewsfeedCore(TitanGraph g) {
+		super();
+		this.g = g;
 	}
 
 }
